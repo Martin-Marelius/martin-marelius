adf111cc4c219dcf7ab81f8a5ec629846bb98c1e
Martin <98266930+Martin-Marelius@users.noreply.github.com>
2024-12-03 19:50:55 +0100

    Fixed metadata issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
-"use client";
-
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Martin Marelius Johnsen - Personal Portfolio',
-  description: 'Personal portfolio showcasing my skills and projects',
-}
 
 export default function RootLayout({
   children,
